5ab579fb915aa04bc62849b7190fb2118f20c1cf
FionaSeahShuHui98 <fionaseah98@gmail.com>
2022-08-02 01:13:56 +0800

    Push Push

--- assets/js/pie_chart_count.js
+++ assets/js/pie_chart_count.js
@@ -1,7 +1,7 @@
 const start = 0
 const end = 12333
 
-d3.select("#hospitalized_count").transition()
+d3.select("hosp_counter").transition()
     .tween("text", () => {
         const interpolator = d3.interpolateNumber(0, end);
         return function(t) {
